0ab0426f576146256a0641e540566cb0c74967a7
sy0725 <sunyong0725@naver.com>
2023-07-24 14:52:35 +0900

    수정

--- client/main.js
+++ client/main.js
@@ -10,38 +10,92 @@ import { diceAnimation, getNode, getNodes } from "./lib/index.js";
 //       - 이벤트 핸들러를 연결한다.
 //       - 애니메이션 코드를 작성한다.
 // 3. 애니메이션 토글 제어 
+// 4. 클로저 + IIFE 를 사용한 변수 보호
+
+// [phase-2] 레코드 리스트 control / view
+// 1. 주사위가 멈추면 기록/초기화 버튼 활성화
+// 2. hidden 속성 제어하기
+// 3. 기록 버튼을 클릭했을때 hidden 제거하기
+// 4. 주사위 값을 가져와서 랜더링
 
 // 배열 구조 분해 할당
 
 
 const [startButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper')
+
+
+// 진짜 진짜 쉬운 과제 (만들어보고 적용도 한번 시켜보자)
+
+// disableElement(node)
+// enableElement(node)
+// isDisableState(note) => true / false
+
+// visibleElement(node)  hidden
+// invisibleElement(node)
+// isVisibleState(node) => true / false
+
+// // function disableElement(node){
+//   recordButton.disabled = false;
+//   resetButton.disabled =false;
+
+//   node.disabled = false;
+
+// // }
+
+// // function enableElement(node){
+//   recordButton.disabled = true;
+//   resetButton.disabled =true;
+// // }
 
 
 
 
-let isClicked = false;
-let stopAnimation;
+const handleRollingDice = ((e) =>{
+  
+  let isClicked = false;
+  let stopAnimation;
+  
 
-function handleRollingDice(e){
+  return () => {  // 주사위 play
+    if(!isClicked){
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled =true;
 
- 
 
-  if(!isClicked){
-    stopAnimation = setInterval(diceAnimation, 100);
+  
+    }else{ //주사위 stop
+      clearInterval(stopAnimation)
+      recordButton.disabled = false;
+      resetButton.disabled =false;
 
-  }else{
-    clearInterval(stopAnimation)
-
+    }
+  
+    isClicked = !isClicked;
   }
 
-  isClicked = !isClicked;
+  
+})()
+
+function handleRecord(){
+  recordListWrapper.hidden = false;
+
+  // 큐브의 data-dice 값만 가져오면 
+
 }
 
 
+function handleReset(){
+  recordListWrapper.hidden = true;
+  recordButton.disabled = true;
+  resetButton.disabled = true;
+
+}
 
 startButton.addEventListener('click',handleRollingDice);
-
-
+recordButton.addEventListener('click',handleRecord)
+resetButton.addEventListener('click',handleReset)
 
 
 
