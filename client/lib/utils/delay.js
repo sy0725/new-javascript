4bc8774b53b7579400e2fb8b57328ff23f0c3962
sy0725 <sunyong0725@naver.com>
2023-07-25 17:31:08 +0900

    추가

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -27,17 +27,60 @@ const second = getNode('.second')
 
 // delayP 함수를 실행하면 리턴되는 값이 promise 객체입니다.
 
-function delayP(){
+// function delayP(){
 
-    return new Promise((resolve, reject) => {
-        resolve('성공입니다!!')
+//     return new Promise((resolve, reject) => {
+//         resolve('성공입니다!!')
+//     })
+
+// }
+
+// delayP()
+// .then((result)=>{
+//     console.log(result);
+// })
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+
+
+
+// 생성자함수 new .... 으로 만드는건 전부 객체가 튀어나온다. JS 특징
+// new promise 는 정확히는 promise 객체를 반환한다.
+// 성공해도 실패해도 .then()은 똑같이 실행되고 (첫번째는 성공했을때 결과, 두번째는 실패했을때 실행되는 결과)
+
+// 1. promise 는 promise 객체를 반환한다.
+
+
+
+let state = false; // 통신 성공, 실패
+
+function delayO(){
+
+    //  성공이야 ? (약속해 알려주기로) 그리고 나서 (then) 이거해
+    //  실패야 ? (약속해 알려주기로) 그리고 나서 (then) 이거해
+
+    return new Promise((resolve,reject)=>{
+
+        // resolve('통신 성공')
+
+        reject('통신 실패')
     })
-
 }
 
-delayP().then((result)=>{
-    console.log(result);
+
+
+delayO()
+.then((결과)=>{
+    console.log(결과);
+},(결과)=>{
+    console.log(결과);
 })
+
+
+
+
 
 
 
